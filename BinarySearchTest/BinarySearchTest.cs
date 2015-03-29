c64f3f5c961a29cf14aa823db3b37564886f2d69
Dmitriy Elisov <dmitry.elisov@phystech.edu>
2015-03-30 01:39:56 +0300

    Math Operations

---
+++ BinarySearchTest/BinarySearchTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace BinarySearchTest
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void EvenLength8_ElementExists_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 4, 6, 7, 8, 10, 12, 14 };
+            int pos = MathOperations.MathOperations.find(arr, 14);
+            Assert.AreEqual(7, pos, "Found incorrectly");
+        }
+
+        [TestMethod]
+        public void Length0_ElementNotExists_ReturnsMinusOne()
+        {
+            List<int> arr = new List<int> { };
+            int pos = MathOperations.MathOperations.find(arr, 5);
+            Assert.AreEqual(-1, pos, "Found incorrectly");
+        }
+
+
+        [TestMethod]
+        public void Length3_LeftElement_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 2, 3 };
+            int pos = MathOperations.MathOperations.find(arr, 1);
+            Assert.AreEqual(0, pos, "Found incorrectly");
+        }
+
+
+        [TestMethod]
+        public void Length3_MiddleElement_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 2, 3 };
+            int pos = MathOperations.MathOperations.find(arr, 2);
+            Assert.AreEqual(1, pos, "Found incorrectly");
+        }
+
+        [TestMethod]
+        public void Length3_RightElement_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 2, 3 };
+            int pos = MathOperations.MathOperations.find(arr, 3);
+            Assert.AreEqual(2, pos, "Found incorrectly");
+        }
+
+        [TestMethod]
+        public void Length4_RightElement_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 2, 3, 4 };
+            int pos = MathOperations.MathOperations.find(arr, 1);
+            Assert.AreEqual(0, pos, "Found incorrectly");
+        }
+
+
+        [TestMethod]
+        public void EvenLength10_ElementExists_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 4, 6, 7, 8, 10, 12, 14, 15, 18 };
+            int pos = MathOperations.MathOperations.find(arr, 7);
+            Assert.AreEqual(3, pos, "Found incorrectly");
+        }
+
+        [TestMethod]
+        public void OddLength9_ElementExists_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 4, 6, 7, 8, 10, 12, 14, 15 };
+            int pos = MathOperations.MathOperations.find(arr, 10);
+            Assert.AreEqual(5, pos, "Found incorrectly");
+        }
+
+
+
+        [TestMethod]
+        public void OddLength7_ElementExists_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 1, 4, 6, 7, 8, 10, 12 };
+            int pos = MathOperations.MathOperations.find(arr, 10);
+            Assert.AreEqual(5, pos, "Found incorrectly");
+        }
+
+        [TestMethod]
+        public void Length1_ElementExists_ReturnsPosition()
+        {
+            List<int> arr = new List<int> { 5 };
+            int pos = MathOperations.MathOperations.find(arr, 5);
+            Assert.AreEqual(0, pos, "Found incorrectly");
+        }
+    }
+}
---
+++ MathOperations/MathOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class MathOperations
+    {
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int binarySearch(int[] arr, int value, int left, int right)
+        {
+            while (left <= right)
+            {
+                int middle = (left + right + 1 ) / 2 + 1;
+                if (arr[middle] == value)
+                    return middle;
+                else if (arr[middle] > value)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+            return -1;
+        }
+
+        public static int find(IEnumerable<int> array, int val)
+        {
+            return binarySearch(array.ToArray(), val, 0, array.Count() - 1);
+        }
+    }
+}
